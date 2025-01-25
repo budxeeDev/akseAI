89f0699ba94aee135293a10802877157bf768996
Budiadnyana <budxee@gmail.com>
2025-01-25 13:38:51 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+$mysql_url = getenv('MYSQL_URL');
+if (!$mysql_url) {
+    die('MYSQL_URL is not set in environment variables.');
+}
+
+$url = parse_url($mysql_url);
+$host = $url['host'];
+$port = $url['port'];
+$user = $url['user'];
+$password = $url['pass'];
+$dbname = ltrim($url['path'], '/');
+
+$conn = new mysqli($host, $user, $password, $dbname, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
